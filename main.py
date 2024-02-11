3ba5fbdf20d7e5377cfedc7bb14a2a16aad7e53a
jltavera <jl.tavera@uniandes.edu.co>
2024-02-10 23:31:21 -0500

    Pokemon comparison

--- main.py
+++ main.py
@@ -1,20 +1,83 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import csv
+import requests
+import json
+from annotated_text import annotated_text
+
+colours = {
+	'normal': '#A8A77A',
+	'fire': '#EE8130',
+	'water': '#6390F0',
+	'electric': '#F7D02C',
+	'grass': '#7AC74C',
+	'ice': '#96D9D6',
+	'fighting': '#C22E28',
+	'poison': '#A33EA1',
+	'ground': '#E2BF65',
+	'flying': '#A98FF3',
+	'psychic': '#F95587',
+	'bug': '#A6B91A',
+	'rock': '#B6A136',
+	'ghost': '#735797',
+	'dragon': '#6F35FC',
+	'dark': '#705746',
+	'steel': '#B7B7CE',
+	'fairy': '#D685AD',
+}
 
 st.title('Pokemon Data Visualization Tool')
 st.divider()
 
-# Load the data
-def load_data():
-    data = pd.read_csv('pokemon.csv')
+#Get a random pokedex number
+random_number = str(np.random.randint(1, 1000))
+random_number_2 = str(np.random.randint(1, 1000))
+
+#Fetch the name of that pokemon 
+def get_random_pokemon(number:int) -> str:
+    try: 
+        url = 'https://pokeapi.co/api/v2/pokemon/'+number
+        response = requests.get(url)
+        data = response.json()
+    except Exception as e:
+        st.write(f'Error: {e}')
+        data = None
+    return data.get('name')
+
+
+poke_name = get_random_pokemon(random_number)
+poke_name_2 = get_random_pokemon(random_number_2)
+
+def get_pokemon_data(pokemon_name) -> dict:
+    try: 
+        url = 'https://pokeapi.co/api/v2/pokemon/'+pokemon_name
+        response = requests.get(url)
+        data = response.json()
+    except Exception as e:
+        st.write(f'Error: {e}')
+        data = None
     return data
 
-data = load_data()
+pokemon_data = get_pokemon_data(poke_name)
+pokemon_data_2 = get_pokemon_data(poke_name_2)
 
-# create a dataframe with the pokemon data
-df = pd.DataFrame(data)
+if pokemon_data and pokemon_data_2:
+    col1, col2 = st.columns(2)
 
-# Display the data
-st.dataframe(df)
+    with col1:
+        st.header(pokemon_data.get('name').capitalize())
+        st.image(pokemon_data.get('sprites').get('front_default'))
+        st.write('Pokemon Weight',pokemon_data.get('weight'))
+        poke_type = pokemon_data.get('types')[0].get('type').get('name') 
+        annotated_text(
+            (poke_type,"", colours[poke_type]),
+        )
+    with col2:
+        st.header(pokemon_data_2.get('name').capitalize())
+        st.image(pokemon_data_2.get('sprites').get('front_default'))
+        st.write('Pokemon Weight',pokemon_data.get('weight'))
+        poke_type = pokemon_data_2.get('types')[0].get('type').get('name') 
+        annotated_text(
+            (poke_type,"", colours[poke_type]),
+        )    
+    
